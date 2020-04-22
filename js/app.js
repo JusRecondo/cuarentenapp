374ea9136f9e762595ed39a64dcab7b984d5bafb
JusRecondo <justina.recondo@gmail.com>
2020-04-22 13:43:38 -0300

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let actividades;
 
 let xhr = new XMLHttpRequest();
 
-xhr.open('GET', './db.json', true);
+xhr.open('GET', 'https://raw.githubusercontent.com/JusRecondo/cuarentenapp/master/db.json', true);
 xhr.responseType = 'text';
 xhr.send();
 
@@ -14,44 +14,46 @@ xhr.onload = function() {
     }
 }
 
+ 
+//sortear y mostrar actividad   
+const button1 = document.querySelector('#sugerencia-btn');
+const sugerencia =  document.querySelector('#sugerencia');
+
 function mostrarActividad(actividad) {   
     return `<div class="card-body">
             <h4 class="card-title">${actividad.nombre}</h4> 
             <p class="card-text">${actividad.descripcion}</p>
             -><a href='${actividad.link}' class='card-link text-white' target="_blank">${actividad.link}</a><-</div>`;
     } 
- 
-//sorteo    
-const button1 = document.getElementById('sugerencia-btn');
 
-button1.onclick =  function() { 
-    let sugerencia =  document.getElementById('sugerencia');
+button1.addEventListener('click', sortearActividad);
 
+function sortearActividad() { 
+   
     if(sugerencia.classList.contains('d-none')){
         sugerencia.classList.remove('d-none');
     } else {  
         sugerencia.innerHTML = '<img class="cat-img" src="./img/cat.png" alt="cat" id="loading">';   
     }
    
-        
-    setTimeout(function(){
-        let sorteo = Math.floor(Math.random() * 27);
+    let sorteo = Math.floor(Math.random() * 27);
         let actividad = actividades[sorteo];
+        
+    setTimeout(function(){     
         sugerencia.innerHTML = mostrarActividad(actividad);  
-    }, 2000)
+    }, 1500)
        
   ;
 };
 
-//form
-const button2 = document.getElementById('form-btn');
+//form 
+const button2 = document.querySelector('#form-btn');
+button2.addEventListener('click', mostrarFormulario);
 
-button2.onclick = function() {
-    let formulario = document.getElementById('formulario');
+function mostrarFormulario() {
+    let formulario = document.querySelector('#formulario');
 
-    formulario.classList.toggle('d-none');
-    
-        
-};
+    formulario.classList.toggle('d-none');      
+}; 
 
 
